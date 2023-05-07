caf3211f84e1df6d3f6d792e4c82dccb367fa93f
erik78se <erik.lonroth@gmail.com>
2023-05-07 20:26:43 +0200

    await

--- update_influxdb.py
+++ update_influxdb.py
@@ -98,7 +98,7 @@ def get_all_endpoints_from_api(rpc_flask_api):
     return all_url_api_tuples
 
 # Main loop
-def main(logger, collect_info_from_endpoint, write_to_influxdb):
+async def main(logger, collect_info_from_endpoint, write_to_influxdb):
     # LOAD CONFIG
     with open('config.json') as f:
         config = json.load(f)
@@ -125,8 +125,9 @@ def main(logger, collect_info_from_endpoint, write_to_influxdb):
         all_url_api_tuples = load_endpoints(rpc_flask_api,cache_refresh_interval=cache_max_age)
 
         # Get block heights from all endpoints asynchronously
-        tasks = [await collect_info_from_endpoint(loop, rpc_request_timeout, url, api_type) for url, api_type in all_url_api_tuples]
-    
+        
+        tasks = [collect_info_from_endpoint(loop, rpc_request_timeout, url, api_type) for url, api_type in all_url_api_tuples]
+                
         info = loop.run_until_complete(asyncio.gather(*tasks))
 
         for endpoint, info_dict in zip(all_url_api_tuples, info):
