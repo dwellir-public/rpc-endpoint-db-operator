25f5d9283bf3fc0ddd33984008ae62043dd21510
erik78se <erik.lonroth@gmail.com>
2023-05-07 20:34:52 +0200

    await

--- update_influxdb.py
+++ update_influxdb.py
@@ -165,4 +165,4 @@ if __name__ == '__main__':
     logger.addHandler(console_handler)
     
     # Main
-    main(logger,collect_info_from_endpoint, write_to_influxdb)
+    asyncio.run(main(logger, collect_info_from_endpoint, write_to_influxdb))
