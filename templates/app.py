5613eee880a8d8602522fecb9edb21e8be4747b9
Jakob Ersson <11717405+jakobilobi@users.noreply.github.com>
2024-11-27 14:35:49 +0100

    chore: allow REST

--- templates/app.py
+++ templates/app.py
@@ -422,7 +422,7 @@ def get_chain_info():
 
 def is_valid_api(api):
     """Test that api string is valid."""
-    return api.lower() in ['substrate', 'ethereum', 'starknet', 'filecoin', 'sui']
+    return api.lower() in ['substrate', 'ethereum', 'starknet', 'filecoin', 'sui', "rest"]
 
 
 def is_valid_url(url):
